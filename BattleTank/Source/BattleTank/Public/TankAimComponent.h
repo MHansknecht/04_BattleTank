8a7f8ea0239e1469595807c4994fbba7bcc99df2
DESKTOP-Michael <Michael.Hansknecht@gmail.com>
2017-10-12 04:26:59 -0400

    BT56 - Enum(erators) in UE4

--- BattleTank/Source/BattleTank/Public/TankAimComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimComponent.h
@@ -8,6 +8,15 @@
 #include "Math/Rotator.h"
 #include "TankAimComponent.generated.h"
 
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8 
+{
+	Reloading,
+	Aiming,
+	Locked,
+};
+
 // Forward Declaration
 class UTankBarrel;
 class UTankTurret;
@@ -29,11 +38,16 @@ public:
 	void SetTurretReference(UTankTurret* TurretToSet);
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
-	
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Reloading;
+
 private:
 	UTankBarrel* Barrel = nullptr;
 	UTankTurret* Turret = nullptr;
 
-	void MoveBarrelTowards(FVector AimDirection);
 
+	void MoveBarrelTowards(FVector AimDirection);
+	
 };
